93644b239c03aecadecee67944e29bb6cdf1d629
Loic Ebbing <ebbing.loic@outlook.com>
2020-08-11 17:18:09 +0200

    ex 3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'about', to: 'games#about'
+  root to: "tasks#index"
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: :task
 end
+
+# index, show, new, create, update, edit
